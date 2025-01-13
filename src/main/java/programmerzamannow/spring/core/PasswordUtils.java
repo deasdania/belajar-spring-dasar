9479b1e72a685319d328a10257986807b023e1ed
deadaniax <dea@x-foundation.io>
2025-01-13 14:09:54 +0700

    use BCrypt logic to hash and checkpassword

--- src/main/java/programmerzamannow/spring/core/PasswordUtils.java
+++ src/main/java/programmerzamannow/spring/core/PasswordUtils.java
@@ -1,6 +1,20 @@
 package programmerzamannow.spring.core;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtils {
+
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    // // Method to hash a plain-text password
+    // public static String hashPassword(String plaintextPassword) {
+    //     return passwordEncoder.encode(plaintextPassword);
+    // }
+
+    // // Method to check if the plain-text password matches the hashed password
+    // public static boolean checkpw(String plaintextPassword, String hashedPassword) {
+    //     return passwordEncoder.matches(plaintextPassword, hashedPassword);
+    // }
 
     // Method to hash a plain-text password
     public static String hashPassword(String plaintextPassword) {
@@ -9,6 +23,8 @@ public class PasswordUtils {
 
     // Method to check if the plain-text password matches the hashed password
     public static boolean checkpw(String plaintextPassword, String hashedPassword) {
+        System.err.println("password:"+ plaintextPassword);
+        System.err.println("hashed:"+ hashedPassword);
         return BCrypt.checkpw(plaintextPassword, hashedPassword);
     }
 }
